00c42ca2214788cfc04617fc0eee5febfd563cbe
ArielLeza <ariel.leza@gmail.com>
2021-04-25 21:05:36 +0300

    removed cert from aaa method

--- aci.tf
+++ aci.tf
@@ -1,7 +1,8 @@
 provider "aci" {
   username    = var.aciUser
-  private_key = var.aciPrivateKey
-  cert_name   = var.aciCertName
+  ## private_key = var.aciPrivateKey
+  password    = var.password
+  ## cert_name   = var.aciCertName
   insecure    = true
   url         = var.aciUrl
 }
